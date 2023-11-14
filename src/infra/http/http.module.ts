c1b73ce9da38518e9a5f71ebe82dab47c6984755
Iann Rodrigues <iannrst11@gmail.com>
2023-11-14 16:11:27 -0300

    feat: Implementando repositórios do prisma

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from '../prisma/prisma.service'
+import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
@@ -13,7 +14,5 @@ import { FetchRecentQuestionsController } from './controllers/fetch-recent-quest
     FetchRecentQuestionsController,
   ],
   // Quais controllers existem dentro desse modulo
-
-  providers: [PrismaService], // Todas as dependências que os controllers podem ter
 })
 export class HttpModule {}
