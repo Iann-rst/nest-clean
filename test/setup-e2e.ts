c77256a1f69d8eb61d182073ba3d7ebe7cfdd5b0
Iann Rodrigues <iannrst11@gmail.com>
2023-12-08 18:02:48 -0300

    feat: Integração com Cloudflare R2 e testando controller de upload

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import 'dotenv/config'
 import { execSync } from 'node:child_process'
 import { randomUUID } from 'node:crypto'
+
+import { config } from 'dotenv'
+
+config({ path: '.env', override: true })
+config({ path: '.env.test', override: true })
 
 const prisma = new PrismaClient()
 
